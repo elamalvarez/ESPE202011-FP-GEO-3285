9b7c2d9d63f49758272cfe62fed488188eb17b05
elamlavarez <elamalvarez2001@gmail.com>
2021-01-08 13:41:11 -0500

    WS01 Hello World in three programming

---
+++ WORKSHOPSS/unit1/WS01Hellowordl.java/SumWorld.java
@@ -0,0 +1,12 @@
+print("hello world form Elam Álvarez \n ESPE \GEO")
+# imput variables
+addend1 = imput('Enter the first number --> ')
+addend2 = imput('Enter the second number --> ')
+
+#add two number
+sum = float(addend1) + float(addend2)
+
+#displaying the sum
+print('The sum of {0} and {1} is {2}' .format(addend1, addend2, sum))
+
+print('The sum is %. 1f' %float(imput('Enter first number: '))+float(imput('Enter second number:'))))
