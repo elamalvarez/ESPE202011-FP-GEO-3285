c0d144aa851cbb3c46d783f16a4f68a5187f78bf
elamlavarez <elamalvarez2001@gmail.com>
2021-02-22 14:15:41 -0500

    Arrays

--- QUIZZ-22-02/Q01_02222021Arrays/src/q01_02222021arrays/Q01_02222021Arrays.java
+++ QUIZZ-22-02/Q01_02222021Arrays/src/q01_02222021arrays/Q01_02222021Arrays.java
@@ -17,33 +17,35 @@ public class Q01_02222021Arrays {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int total0fPlayers = 0;
-        String [] players;
-        float goalAverages;
-        
-        Scanner scanner = new Scanner (System.in);
-        
-        System.out.print("Please enter the amount of players you wnat to record --> ");
-        total0fPlayers = scanner.nextInt();
-        
-        players = new String [total0fPlayers];
-        goalAverages = new float [total0fPlayers];
-        
-        for(int i = 0; i < total0fPlayers; i++) { 
-            System.out.print("Please enter the name of player #" + i + "-->");
+        int totalOfPlayers = 0;
+        String[] players;
+        float[] goalAverages;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Please Enter the amount of players you want to record -> ");
+        totalOfPlayers = scanner.nextInt();
+
+        players = new String[totalOfPlayers];
+        goalAverages = new float[totalOfPlayers];
+
+        for (int i = 0; i < totalOfPlayers; i++) {
+            scanner.nextLine();
+            System.out.print(" Please Enter the name of player # " + i + " -> ");
             players[i] = scanner.nextLine();
-            System.out.print("please enter " + players [i] + "goal average --> ");
+            System.out.print(" Please enter " + players[i] + " goals average ->");
             goalAverages[i] = scanner.nextFloat();
-            
         }
-        
-        System.out.print("Player --->   \t " + goalAverages );
-        for(int i = 0; i < total0fPlayers; i++) { 
-        System.out.println(players [i] + "--> \t " goalAverages[i]);
-        
-        
+
+        System.out.println(" Player    ->  \t     goal average ");
+
+        for (int i = 0; i < totalOfPlayers; i++) {
+            System.out.println(players[i] + " ->  \t " + goalAverages[i]);
+        }
+
     }
-    
+
 }
-}
+        
+      
 
